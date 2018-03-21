902829d898af03d04f73ae2999b86dee02f5e025
Nguyễn Minh Đức <duc2820@gmail.com>
2018-03-21 09:33:08 +0700

    Update Code-javascript-good.js

--- Code-javascript-good.js
+++ Code-javascript-good.js
@@ -25,3 +25,48 @@ function addRemove(col) {
 let arr=addRemove([3,4,5]);
 let afterArr=arr(45); // arr:[3,4,5,45]
 afterArr(); // arr:[3,4,5]
+//example about get error
+// from this api below
+{
+    "errors": {
+        "email": [
+            "can't be blank"
+        ],
+        "password": [
+            "can't be blank"
+        ],
+        "username": [
+            "can't be blank",
+            "is too short (minimum is 1 character)",
+            "is too long (maximum is 20 characters)"
+        ]
+    }
+}
+// i can list all error by the way
+function mapValuesToList(obj, fn) {
+    let ret = [];
+
+    for (var k in obj) {
+        if (obj.hasOwnProperty(k)) {
+            ret.push(fn(obj[k], k));
+        }
+    }
+
+    return ret;
+  
+}
+//code in reactjs
+render
+(
+  <ul className="error-messages">
+           
+            
+            {
+                mapValuesToList(errors, (errList, field) => (
+                errList.map((errMessage, i) => (
+                    <li key={i}>{field} {errMessage}</li>
+                ))
+            ))
+}
+     </ul>
+)
